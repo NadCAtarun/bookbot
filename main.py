15626c3feccdf2788d6c0c5673297f15c6186733
NadCAtarun <nadc.atarun@gmail.com>
2025-04-01 14:01:58 +0200

    Update code to comply with new instructions

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import sys
+from stats import count_words
 
 def read_book(book_path):
     with open(book_path) as f:
         return f.read()
 
-def count_words(book):
-    print(f"{len(book.split())} words found in the document")
-
 def count_characters(book):
+    print("--------- Character Count -------")
     chars = {}
     for c in book.lower():
         if not c.isalpha():
@@ -16,15 +16,18 @@ def count_characters(book):
         else:
             chars[c] = 1
     for c, n in sorted(chars.items(), key=lambda item: item[1], reverse=True):
-        print(f"The '{c}' character was found {n} times")
+        print(f"{c}: {n}")
 
 def main():
-    book_path = f"books/frankenstein.txt"
-    print(f"--- Begin report of {book_path} ---")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
     book = read_book(book_path)
     count_words(book)
-    print()
     count_characters(book)
-    print("--- End report ---")
+    print("============= END ===============")
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(book):
+    print("----------- Word Count ----------")
+    print(f"Found {len(book.split())} total words")
+
